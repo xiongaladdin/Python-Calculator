c857b6ac4b2567f1d8f6b88c0dae16a500f72882
xiongaladdin <144748066+xiongaladdin@users.noreply.github.com>
2025-06-04 13:10:05 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,33 @@
+# Python Calulator
+
+operator = input("Enter an operator (+ - * /): ")
+
+while True:
+    try:
+        num1 = int(input("Enter the 1st number: "))
+        break
+    except ValueError:
+        print("Please enter in a number.")
+
+while True:
+    try:
+        num2 = int(input("Enter the 2nd number: "))
+        break
+    except ValueError:
+        print("Please enter in a number: ")
+
+# print(num1 + num2)
+
+if operator == "+":
+    print(num1 + num2)
+elif operator == "-":
+    print(num1 - num2)
+elif operator == "*":
+    print(num1 * num2)
+elif operator == "/":
+    if num2 == 0:
+        print("Cannot divide by zero.")
+    else:
+        print(num1 / num2)
+else:
+    print("Invalid operator.")
